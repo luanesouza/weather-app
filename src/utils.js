1ddf68da364788a1fbfa551cb961e20469329a4a
luanesouza <luanesouza411@gmail.com>
2020-01-18 11:50:51 -0500

    api call returns 200

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,17 @@
 const axios = require('axios')
 
-const fetchWeather = cityName => {
-  const API_KEY = "d29e9cce44012ae03806fcd9edc39a4e";
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+const API_KEY = 'd29e9cce44012ae03806fcd9edc39a4e';
+
+const fetchWeather = async cityName => {
   // TODO: fetch weather forecast from endpoint
   // from https://openweathermap.org/api
+  try {
+    const response = await axios.get(`${BASE_URL}q=${cityName},us&APPID=${API_KEY}`);
+    console.log(response.data);
+  } catch(e) {
+    console.error(e.message);
+  }
 };
+
+fetchWeather('Brooklyn')
